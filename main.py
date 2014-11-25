23c74e09e8dae93e2f7c05dbc7e01018a47e9ab2
StephHippo <stephaniehippo@gmail.com>
2014-11-24 20:04:04 -0500

    Syncing.

--- main.py
+++ main.py
@@ -17,35 +17,59 @@ dealer_switch_probability = {
 
 flip_probability = {
     'F' : {'H':0.5, 'T':0.5},
-    'B' : {'H':0.75, 'T':0.75}
+    'B' : {'H':0.75, 'T':0.25}
 }
 casino_markov = HMM(flips,coins,coin_start_probability,dealer_switch_probability,flip_probability)
 #Run Viterbi
 casino = Viterbi_Matrix(casino_markov)
 casino.viterbi()
 casino.print_table()
-print casino.probability_at_ith_observation(7,'F')
+print casino.viterbi_path
+casino.backwards_probability()
+casino.print_posterior_matrix()
 
-states = ('H', 'F')
-observation = ["Normal", "Cold", "Dizzy"]
-start_probability = {
-    "H" : 0.6,
-    "F" : 0.4
-}
-transition_probability = {
-    "H" : {"H": 0.7, "F": 0.3},
-    "F" : {"H": 0.4, "F" : 0.6}
-}
-emission_probability = {
-    "H" : {"Dizzy": 0.1, "Cold": 0.4, "Normal": 0.5},
-    "F" : {"Dizzy": 0.6, "Cold": 0.3, "Normal": 0.1}
-}
+# states = ('H', 'F')
+# observation = ["Normal", "Cold", "Dizzy"]
+# start_probability = {
+#     "H" : 0.6,
+#     "F" : 0.4
+# }
+# transition_probability = {
+#     "H" : {"H": 0.7, "F": 0.3},
+#     "F" : {"H": 0.4, "F" : 0.6}
+# }
+# emission_probability = {
+#     "H" : {"Dizzy": 0.1, "Cold": 0.4, "Normal": 0.5},
+#     "F" : {"Dizzy": 0.6, "Cold": 0.3, "Normal": 0.1}
+# }
+#
+# #Declare New HMM
+# healthy_markov = HMM(observation, states,start_probability,transition_probability,emission_probability)
+# #Run Viterbi
+# healthy = Viterbi_Matrix(healthy_markov)
+# healthy.viterbi()
+# healthy.print_table()
+# print healthy.probability_at_ith_observation(3,'F')
+# print healthy.viterbi_path
 
-#Declare New HMM
-healthy_markov = HMM(observation, states,start_probability,transition_probability,emission_probability)
-#Run Viterbi
-healthy = Viterbi_Matrix(healthy_markov)
-healthy.viterbi()
-healthy.print_table()
-print healthy.probability_at_ith_observation(3,'F')
-
+# states = ('R', 'D') #Raining and Dry
+# observation = ["U", "U", "N", "U", "U"]
+# start_probability = {
+#     "R": .5,
+#     "D": .5
+# }
+# transition_probability = {
+#     "R": {"R": .7, "D": .3},
+#     "D": {"R": .3, "D": .7}
+# }
+# emission_probability = {
+#     "R": {"U": .9, "N": .1},
+#     "D": {"U": .2, "N": .8}
+# }
+# rainy_markov = HMM(observation, states,start_probability,transition_probability,emission_probability)
+# rainy = Viterbi_Matrix(rainy_markov)
+# rainy.viterbi()
+# rainy.print_table()
+# print rainy.viterbi_path
+# rainy.backwards_probability()
+# rainy.print_posterior_matrix()
--- viterbi_matrix.py
+++ viterbi_matrix.py
@@ -3,8 +3,9 @@ from HMM import HMM
 class Viterbi_Matrix:
     def __init__(self,hidden_markov_model):
         self.hmm = hidden_markov_model
-        self.matrix = {}
+        self.forward_matrix = {}
         self.viterbi_path = [None] * len(self.hmm.observations)
+        self.posterior_matrix = {}
 
     def viterbi(self):
         #initialize start probabilities in first column
@@ -16,34 +17,31 @@ class Viterbi_Matrix:
                 max = 0
                 for state in self.hmm.states:
                     # for emission in self.hmm.emission_prob[state]:
-                    prev_prob = self.matrix[state][i]
+                    prev_prob = self.forward_matrix[state][i]
                     trans_prob = self.hmm.transition_prob[state][transition]
                     observe = self.hmm.observations[i+1]
                     emis_prob = self.hmm.emission_prob[transition][observe]
                     candidate = prev_prob * trans_prob * emis_prob
                     if candidate > max:
                         max = candidate
-                self.matrix[transition][i+1] = max
+                self.forward_matrix[transition][i+1] = max
                 if max > highest_prob:
                     highest_prob = max
                     top_state = transition
             self.viterbi_path[i+1] = top_state
 
-
-
-
     def __initialize_matrix(self):
         start = None
         for state in self.hmm.start_prob:
-            self.matrix[state] = [None] * (len(self.hmm.observations))
-            self.matrix[state][0] = self.hmm.start_prob[state] * self.hmm.emission_prob[state][self.hmm.observations[0]]
-            if self.matrix[state][0] > start:
+            self.forward_matrix[state] = [None] * (len(self.hmm.observations))
+            self.forward_matrix[state][0] = self.hmm.start_prob[state] * self.hmm.emission_prob[state][self.hmm.observations[0]]
+            if self.forward_matrix[state][0] > start:
                 start = state
         self.viterbi_path[0]=start
 
     def __validate_matrix_filled(self):
-        for key in self.matrix:
-            for value in self.matrix[key]:
+        for key in self.forward_matrix:
+            for value in self.forward_matrix[key]:
                 if value == None:
                     raise "Matrix needs filled out. Run Viterbi first"
 
@@ -54,6 +52,7 @@ class Viterbi_Matrix:
         for observation in self.hmm.observations:
             header += "\t|"
             header += observation
+            header += "\t"
         table+=header+"\n"
         body = ""
         for state in self.hmm.states:
@@ -62,15 +61,54 @@ class Viterbi_Matrix:
             body += "\t"
             for observation in xrange(len(self.hmm.observations)):
                 body += "\t|"
-                body += str(round(self.matrix[state][observation],4))
+                body += str(round(self.forward_matrix[state][observation],4))
             body += "\n"
         table += body
         print table
 
+    def backwards_probability(self):
+        #initialize posterior matrix
+            #for each state
+            #allocate an array of length observations+1
+        for state in self.hmm.states:
+            self.posterior_matrix[state] = [None]*(len(self.hmm.observations)+1)
+            self.posterior_matrix[state][-1] = 1
+        #for each observation, starting from the end
+        for i in xrange(len(self.hmm.observations)): #fill out each col of the table
+            for transition in self.hmm.transition_prob: #for each row in the table
+                #for observation in reversed(self.hmm.observations):
+                observation = self.hmm.observations[-i]
+                probability = 0
+                for state in self.hmm.states:
+                    #get the previous probability
+                    prev_prob = self.posterior_matrix[state][-(i+1)]
+                    #the transition probability
+                    trans_prob = self.hmm.transition_prob[state][transition]
+                    #the emission probability
+                    emission_prob = self.hmm.emission_prob[state][observation]
+                    probability += prev_prob * trans_prob * emission_prob
+                self.posterior_matrix[transition][-(i+2)]=probability
+
+    def print_posterior_matrix(self):
+        table = ""
+        header = "\t\t"
+        for observation in self.hmm.observations:
+            header += "\t|"
+            header += observation
+            header += "\t"
+        table += header+"\n"
+        body = ""
+        for state in self.hmm.states:
+            body += "\t|"
+            body += state
+            body += "\t"
+            for observation in xrange(len(self.hmm.observations)):
+                body += "\t|"
+                body += str(round(self.posterior_matrix[state][observation],4))
+            body += "\n"
+        table += body
+        print table
 
     def probability_at_ith_observation(self, i, state):
         self.__validate_matrix_filled()
-        return self.matrix[state][i-1]
-
-    def print_path(self):
-        return self.viterbi_path
+        return self.forward_matrix[state][i-1]
